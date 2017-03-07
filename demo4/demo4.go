16835ea115eedc2cca7d4bf1e7f724fd182f4374
chenlin <chenlin@gepingtech.com>
2017-03-07 16:21:06 +0800

    fix bugs

--- demo4/demo4.go
+++ demo4/demo4.go
@@ -51,6 +51,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		return t.read(stub, args)
 	case "read2":
 		return t.read2(stub, args)
+	case "read3":
+		return t.read3(stub)
 	case "helloworld":
 		return t.helloworld()
 	default:
@@ -77,6 +79,23 @@ func (t *SimpleChaincode) helloworld() ([]byte, error) {
 	return result, nil
 }
 
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+	if len(args) != 1 {
+		return nil, errors.New("incorrect number of arguments. expecting 1")
+	}
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
 func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, value string
 	var err error
@@ -96,23 +115,4 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	}
 
 	return nil, nil
-}
-
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err error
-
-	if len(args) != 1 {
-		return nil, errors.New("incorrect number of arguments. expecting 1")
-	}
-
-	key = args[0]
-	valAsbytes, err := stub.GetState(key)
-
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	return valAsbytes, nil
 }

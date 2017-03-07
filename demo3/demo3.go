519fc387a1226fb5c90e24ad9a33678270598092
chenlin <chenlin@gepingtech.com>
2017-03-07 09:15:59 +0800

    step1

--- demo3/demo3.go
+++ demo3/demo3.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
+type SimpleChaincode struct {
+}
+
 func main() {
+	err := shim.Start(new(SimpleChaincode))
+
 	fmt.Println("hello world!")
 	fmt.Println("math.Pi is: ", math.Pi)
 }

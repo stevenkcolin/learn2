a705887c4121d026f04f5f33ef608c5219043be6
chenlin <chenlin@gepingtech.com>
2017-03-08 15:44:57 +0800

    updates init()

--- demo5/demo5.go
+++ demo5/demo5.go
@@ -31,7 +31,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("invalid admin certificate. Empty")
 	}
 	stub.PutState("admin", adminCert)
-	return nil, nil
+	return adminCert, nil
 }
 
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {

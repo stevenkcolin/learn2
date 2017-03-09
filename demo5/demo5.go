0ce8fe320f54b9c2ecb92e94bb90757ea8ad3462
chenlin <chenlin@gepingtech.com>
2017-03-09 18:54:57 +0800

    test

--- demo5/demo5.go
+++ demo5/demo5.go
@@ -232,12 +232,20 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		rowChannel, _ := stub.GetRows("AssetsOwnership", columns)
 
 		var rows []shim.Row
-		for row := range rowChannel {
-			val0 := row.Columns[0].GetString_()
-			val1 := row.Columns[1].GetString_()
-			fmt.Printf("val0 is: %v, val1 is: %v", val0, val1)
-			rows = append(rows, row)
+		for {
+			select {
+			case row := <-rowChannel:
+				val0 := row.Columns[0].GetString_()
+				val1 := row.Columns[1].GetString_()
+				fmt.Printf("val0 is: %v, val1 is: %v", val0, val1)
+				rows = append(rows, row)
+			}
+
+			if rowChannel == nil {
+				break
+			}
 		}
+
 		// for {
 		// 	select {
 		// 	case row, ok := <-rowChannel:

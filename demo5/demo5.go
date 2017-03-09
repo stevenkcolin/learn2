0e12186f4202c57d807195d10251e90654c6d2fc
chenlin <chenlin@gepingtech.com>
2017-03-09 19:14:26 +0800

    test

--- demo5/demo5.go
+++ demo5/demo5.go
@@ -232,15 +232,28 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		rowChannel, _ := stub.GetRows("AssetsOwnership", columns)
 
 		var rows []shim.Row
-		for i := 0; i < 10; i++ {
-			select {
-			case row := <-rowChannel:
-				val0 := row.Columns[0].GetString_()
-				val1 := row.Columns[1].GetString_()
-				fmt.Printf("val0 is: %v, val1 is: %v", val0, val1)
-				rows = append(rows, row)
-			}
-		}
+
+		row := <-rowChannel
+		val0 := row.Columns[0].GetString_()
+		val1 := row.Columns[1].GetString_()
+		fmt.Printf("val0 is: %v, val1 is: %v", val0, val1)
+		rows = append(rows, row)
+
+		row = <-rowChannel
+		val0 = row.Columns[0].GetString_()
+		val1 = row.Columns[1].GetString_()
+		fmt.Printf("val0 is: %v, val1 is: %v", val0, val1)
+		rows = append(rows, row)
+
+		// for i := 0; i < 10; i++ {
+		// 	select {
+		// 	case row := <-rowChannel:
+		// 		val0 := row.Columns[0].GetString_()
+		// 		val1 := row.Columns[1].GetString_()
+		// 		fmt.Printf("val0 is: %v, val1 is: %v", val0, val1)
+		// 		rows = append(rows, row)
+		// 	}
+		// }
 
 		// for {
 		// 	select {

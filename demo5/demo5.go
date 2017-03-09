215a5b3a7d131fdb10f130c2fcc2a9f0c98742ec
chenlin <chenlin@gepingtech.com>
2017-03-09 13:10:13 +0800

    updated

--- demo5/demo5.go
+++ demo5/demo5.go
@@ -198,20 +198,23 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		if len(args) != 2 {
 			return nil, errors.New("incorrect args")
 		}
-		asset := args[0]
+		// asset := args[0]
 		owner := []byte(args[1])
 
-		col1 := shim.Column{Value: &shim.Column_String_{String_: asset}}
+		// col1 := shim.Column{Value: &shim.Column_String_{String_: asset}}
+		// col2 := shim.Column{Value: &shim.Column_Bytes{Bytes: owner}}
+
 		col2 := shim.Column{Value: &shim.Column_Bytes{Bytes: owner}}
-		var columns []shim.Column
-		columns = append(columns, col1)
-		columns = append(columns, col2)
+		// var columns []shim.Column
+		// columns = append(columns, col1)
+		// columns = append(columns, col2)
 
 		// val0 := columns[0].GetString_()
 		// val1 := columns[1].GetString_()
-		result := asset + "********" + string(owner) + "*****end of input ****"
-		result += col1.GetString_() + "********" + col2.GetString_()
-		return []byte(result), nil
+		// result := asset + "********" + string(owner) + "*****end of input ****"
+		// result += col1.GetString_() + "****" + col2.GetString_()
+		result := col2.GetBytes()
+		return result, nil
 	}
 
 	return nil, nil

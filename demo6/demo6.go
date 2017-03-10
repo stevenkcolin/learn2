9fb6befa6bba0824e24b63956e7c0bf2b8e24d6a
chenlin <chenlin@gepingtech.com>
2017-03-10 14:50:57 +0800

    demo6.go

--- demo6/demo6.go
+++ demo6/demo6.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -19,23 +20,38 @@ func main() {
 	}
 }
 
+var projectName string
+var projectRate float64
+var projectPeriod, projectGoal, projectTimes int
+var projectBenfiary string
+var projectState string
+
 //Init function
 //Step1: 获得调用init()的caller, 并且保存在"admin"中
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Init()")
 
+	//get caller's certificate, and saved to "admin" state
 	fmt.Println("started get caller's metatdat")
 	adminCert, err := stub.GetCallerCertificate()
-
 	if err != nil {
 		return nil, errors.New("failed getting metadata")
 	}
 	if len(adminCert) == 0 {
 		return nil, errors.New("Invalid admin certificate. Empty.")
 	}
-
 	fmt.Printf("the administrator is [%v]", adminCert)
 	stub.PutState("admin", adminCert)
+
+	//started to initialize project
+	projectName = args[0]                            //项目名称
+	projectRate, _ = strconv.ParseFloat(args[1], 64) //项目年化利率
+	projectPeriod, _ = strconv.Atoi(args[2])         //项目天数
+	projectGoal, _ = strconv.Atoi(args[3])           //项目目标募集金额
+	projectTimes = 1                                 //项目期数
+	projectBenfiary = args[4]                        //项目受益人
+	projectState = "Draft"                           //项目当前状态
+
 	return nil, nil
 }
 
@@ -63,7 +79,19 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		if err != nil {
 			return nil, errors.New("Failed in function getState")
 		}
-		return result, err
+		return result, err //end of getState
+	case "getProjectState":
+		fmt.Println("started in function getProjectState")
+		result += projectName + "/"
+		result += string(projectRate) + "/"
+		result += string(projectPeriod) + "/"
+		result += string(projectGoal) + "/"
+		result += string(projectTimes) + "/"
+		result += projectBenfiary + "/"
+		result += projectState
+
+		return result, nil
+
 	}
 	return nil, nil
 }

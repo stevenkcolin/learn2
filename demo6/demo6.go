3f1f6bde92066f550d829d2f91cfa355157def16
chenlin <chenlin@gepingtech.com>
2017-03-10 12:54:10 +0800

    ++++++ init() ++++++ Query.getState()

--- demo6/demo6.go
+++ demo6/demo6.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
+// SimpleChaincode basic struct
 type SimpleChaincode struct {
 }
 
@@ -17,20 +19,47 @@ func main() {
 	}
 }
 
+//Init function
+//Step1: 获得调用init()的caller, 并且保存在"admin"中
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Init()")
 
+	fmt.Println("started get caller's metatdat")
+	adminCert, err := stub.GetCallerMetadata()
+
+	if err != nil {
+		return nil, errors.New("failed getting metadata")
+	}
+
+	stub.PutState("admin", adminCert)
 	return nil, nil
 }
 
+// Invoke function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Invoke()")
 
 	return nil, nil
 }
 
+//Query funciton
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Query()")
 
+	switch function {
+	case "getState":
+		fmt.Println("started in function getState()")
+		if len(args) != 1 {
+			return nil, errors.New("incorrect args")
+		}
+
+		key := args[0]
+		result, err := stub.GetState(key)
+
+		if err != nil {
+			return nil, errors.New("Failed in function getState")
+		}
+		return result, err
+	}
 	return nil, nil
 }

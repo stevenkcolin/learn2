f7ca663afb75c7dabd485f3bf6877527f8efb05f
chenlin <chenlin@gepingtech.com>
2017-03-10 12:26:45 +0800

    demo6.go

--- demo6/demo6.go
+++ demo6/demo6.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/op/go-logging"
 )
 
 type SimpleChaincode struct {
 }
 
-// var myLogger = logging.MustGetLogger("asset_mgm")
+var myLogger = logging.MustGetLogger("asset_mgm")
 
 func main() {
 	fmt.Println("started logging in main()")
@@ -21,16 +22,18 @@ func main() {
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Init()")
-	// myLogger.Debug("Init Chaincode...")
+	myLogger.Debug("init chaincode")
 	return nil, nil
 }
 
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Invoke()")
+	myLogger.Debug("start Invoke")
 	return nil, nil
 }
 
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Query()")
+	myLogger.Debug("started logging in Query()")
 	return nil, nil
 }

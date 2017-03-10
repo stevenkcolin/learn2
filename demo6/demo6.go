9e68b2a9d988f09bb9860e61ef3ce7cb231d8bfd
chenlin <chenlin@gepingtech.com>
2017-03-10 12:41:37 +0800

    remove op/logging code

--- demo6/demo6.go
+++ demo6/demo6.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/op/go-logging"
 )
 
 type SimpleChaincode struct {
 }
-
-var myLogger = logging.MustGetLogger("asset_mgm")
 
 func main() {
 	fmt.Println("started logging in main()")
@@ -22,18 +19,18 @@ func main() {
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Init()")
-	myLogger.Debug("init chaincode")
+
 	return nil, nil
 }
 
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Invoke()")
-	myLogger.Debug("start Invoke")
+
 	return nil, nil
 }
 
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Query()")
-	myLogger.Debug("started logging in Query()")
+
 	return nil, nil
 }

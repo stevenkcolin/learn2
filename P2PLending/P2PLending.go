7e96a135de5b6aeb3c866ec7276cb4f696f04e61
chenlin <chenlin@gepingtech.com>
2017-03-12 04:12:58 +0800

    ++++++ add code to fund init()

--- P2PLending/P2PLending.go
+++ P2PLending/P2PLending.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
+
+var projectName string
+var projectRate int
+var projectPeriod int
+var projectGoal int
+var projectTimes int
+var projectBenifary string
+var projectState string
+var currentPrice float64
+var projectSummary int
+var projectDeadline int64
+var shareList map[string]int
+var isProjectStarted bool
+var isDue bool
+var isFinished bool
+
+var availableList map[string]float64
 
 type SimpleChaincode struct {
 }
@@ -20,6 +39,35 @@ func main() {
 //Init comment
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("started logging in Init()")
+	//step1: initialize the ShareList & AvailableList
+	shareList = make(map[string]int)
+	availableList = make(map[string]float64)
+	//step2: check whether args == 5
+	if len(args) != 5 {
+		return nil, errors.New("failed in args")
+	}
+
+	//step3: initialize the project properties
+	projectName = args[0]
+	projectRate, _ = strconv.Atoi(args[1])
+	if projectRate <= 0 {
+		return nil, errors.New("errors in args[1], it cannot be negative")
+	}
+	projectPeriod, _ = strconv.Atoi(args[2])
+	if projectPeriod <= 0 {
+		return nil, errors.New("errors in args[2], it cannot be negative")
+	}
+	projectGoal, _ = strconv.Atoi(args[3])
+	if projectGoal <= 0 {
+		return nil, errors.New("errors in args[3], it cannot be negative")
+	}
+	projectTimes = 1
+	projectBenifary = args[4]
+	projectState = "public"
+	currentPrice = 1.0
+	projectSummary = 0
+	projectDeadline = 9999999999
+
 	return nil, nil
 }
 
